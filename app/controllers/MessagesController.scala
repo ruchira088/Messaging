371afe804aa9b1c5f13264a9549cab37722b4eeb
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2016-06-01 10:27:56 +1000

    Generalizing the getMessages method

--- app/controllers/MessagesController.scala
+++ app/controllers/MessagesController.scala
@@ -3,14 +3,15 @@ package controllers
 import javax.inject.Inject
 
 import models.Message
-import play.api.libs.json.Json
+import controllers.MessagesController.toJson
+import play.api.libs.json.{JsObject, Json}
 import play.api.mvc._
 import play.modules.reactivemongo.json._
 import play.modules.reactivemongo.{MongoController, ReactiveMongoApi, ReactiveMongoComponents}
 import reactivemongo.api.ReadPreference
 import reactivemongo.play.json.collection.JSONCollection
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 class MessagesController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(implicit context : ExecutionContext) extends Controller
   with MongoController with ReactiveMongoComponents
@@ -19,15 +20,18 @@ class MessagesController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(impli
 
   def getMessages(receiver: String) = Action.async
   {
-    collection.flatMap
-    {
-      _.find(Json.obj("receiver" -> receiver))
-        .cursor[Message](ReadPreference.primary)
-          .collect[List]()
-    }.map
-    {
-      data => Ok(Json.toJson(data))
-    }
+    request =>
+      {
+        find(request.queryString)
+          .map(data => Ok(Json.toJson[List[Message]](data)))
+      }
+  }
+
+  def find(json: JsObject) : Future[List[Message]] = collection.flatMap
+  {
+    _.find(json)
+      .cursor[Message](ReadPreference.primary)
+      .collect[List]()
   }
 
   def sendMessage = Action.async
@@ -59,5 +63,22 @@ class MessagesController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(impli
           }
         }
     }
+  }
+}
+
+object MessagesController
+{
+  implicit def toJson(parameters: Map[String, Seq[String]]): JsObject =
+  {
+    Json.toJson(parameters.map
+          {
+            value =>
+            {
+              val (key, values) = value
+
+              (key, if(values.length == 1) Json.toJson(values(0)) else Json.toJson(values))
+            }
+          })
+      .as[JsObject]
   }
 }
